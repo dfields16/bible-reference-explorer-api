15b3144ae5ef6109889a025f9f27709e19b52625
Dawson Fields <dawson@gofields.com>
2025-09-11 17:42:59 -0500

    Fix Song of Songs

--- src/main/java/com/bible/reference/explorer/api/Components/VerseRepository.java
+++ src/main/java/com/bible/reference/explorer/api/Components/VerseRepository.java
@@ -90,6 +90,10 @@ public class VerseRepository {
 	}
 
 	public Query getVerseQuery(String verseId, int limit) {
+		if(verseId.contains("Song of Songs")) {
+			verseId = verseId.replace("of Songs", "");
+		}
+
 		return new Query(
 				"CALL{" +
 						"	MATCH (v:Verse)-[rel:references]-(p:Verse) WHERE v.title = $verseTitle" +
